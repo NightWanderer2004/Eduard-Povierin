20dab5aea29220df353012688946e9532ef3ebc5
Eduard <goby145csgo@gmail.com>
2022-11-27 21:37:56 +0100

    add config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 module.exports = config => {
-   config.addPassthroughCopy('src/posts')
+   config.addNunjucksFilter('getAge', date => {
+      const diff = Date.now() - date.getTime()
+      const age = new Date(diff)
+      return Math.abs(age.getUTCFullYear() - 1970)
+   })
+   config.addNunjucksFilter('test', name => `hello, ${name}`)
+
    config.addPassthroughCopy('src/assets')
    config.addPassthroughCopy('src/scripts')
+   config.addWatchTarget('src/**/*.{css, html, js}')
 
    return {
       dir: {
@@ -9,6 +16,7 @@ module.exports = config => {
          output: 'dist',
          includes: 'includes',
          layouts: 'layouts',
+         data: 'data',
       },
    }
 }
