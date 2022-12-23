bc2b174172e692ec2e196e1b3d2457efd5534f95
Eduard <goby145csgo@gmail.com>
2022-12-23 23:36:17 +0100

    improve arrow

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -1,6 +1,14 @@
-const elem = document.querySelector('.name')
+const myName = document.querySelector('.name')
+const arrowBtn = document.querySelector('.arrowBtn')
+
 window.addEventListener('scroll', () => {
    const scrolled = window.pageYOffset
-   elem.style.transform = `translateY(-${0.4 * scrolled}%)`
-   // elem.style.opacity = 1 - document.documentElement.scrollTop / 700
+   myName.style.transform = `translateY(-${0.4 * scrolled}%)`
+})
+arrowBtn.addEventListener('click', el => {
+   const aboutOffsetTop = document.querySelector('.about').offsetTop
+   scroll({
+      top: aboutOffsetTop - 15,
+      behavior: 'smooth',
+   })
 })
