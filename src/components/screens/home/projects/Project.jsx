2589857c97ef8be5e79b10165d1dcfa7d63bc130
Eduard <goby145csgo@gmail.com>
2023-04-14 00:29:42 +0200

    update view

--- src/components/screens/home/projects/Project.jsx
+++ src/components/screens/home/projects/Project.jsx
@@ -1,37 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import s from './Projects.module.scss'
 
 const Project = ({ title, img, url, desc }) => {
-   const [inView, setInView] = useState(false)
-   const elemRef = useRef(null)
-
-   const options = {
-      rootMargin: '-70px',
-      threshold: 0.4,
-   }
-
-   useEffect(() => {
-      const observer = new IntersectionObserver(entries => {
-         entries.forEach(entry => {
-            if (entry.isIntersecting) setInView(true)
-            else setInView(false)
-         })
-      }, options)
-
-      if (elemRef.current) observer.observe(elemRef.current)
-
-      return () => {
-         if (elemRef.current) observer.unobserve(elemRef.current)
-      }
-   }, [])
-
    return (
-      <li ref={elemRef} className={`${s.project} ${inView ? s.inView : ''}`}>
-         <Image src={img} alt={title} width='828' height='1438' />
-         <a href={url} className={s.backdrop}>
-            <p className={s.description}>{desc}</p>
-            <p className={s.visit}>visit</p>
+      <li className={`${s.project}`}>
+         <a href={url} target='_blank'>
+            <Image src={img} alt={title} width='828' height='1438' />
+            <p>{desc}</p>
          </a>
       </li>
    )
