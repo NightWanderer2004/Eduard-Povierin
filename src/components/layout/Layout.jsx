2d7af41ccd4db79cfb408a99c17b27f96414e524
Eduard <goby145csgo@gmail.com>
2023-04-14 22:22:21 +0200

    add dark theme

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Footer from '../footer/Footer'
 import Navbar from '../navbar/Navbar'
 import Meta from '../seo/Meta'
 
 const Layout = ({ title, description, children }) => {
+   const [isDarkMode, setIsDarkMode] = useState(false)
+
+   useEffect(() => {
+      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+      const listener = e => setIsDarkMode(e.matches)
+
+      mediaQuery.addEventListener('change', listener)
+      setIsDarkMode(mediaQuery.matches)
+
+      return () => mediaQuery.removeEventListener('change', listener)
+   }, [])
+
    return (
       <>
          <Meta title={title} description={description} />
-         <div className='app flex flex-col min-h-screen overflow-hidden'>
+         <div className={`app flex flex-col min-h-screen overflow-hidden bg-background ${isDarkMode ? 'dark' : ''}`}>
             <Navbar />
             <div className='container mx-auto px-3 flex-1 mt-36 sm:mt-[190px]'>{children}</div>
             <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@
 module.exports = {
    content: ['./src/**/*.{js,ts,jsx,tsx}'],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            background: 'var(--bg-color)',
+            blur: 'var(--blur-color)',
+            primary: 'var(--text-color)',
+            'link-nav': 'var(--link-nav-color)',
+            link: 'var(--link-color)',
+         },
+      },
    },
    plugins: [],
 }
