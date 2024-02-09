e76cc7f3baea8eae20855b05e2c5f0be9bc5e0d0
Eduard Povierin <edpovierin@gmail.com>
2024-02-09 15:33:03 +0100

    Fix image path

--- src/components/screens/about/env/Environment.jsx
+++ src/components/screens/about/env/Environment.jsx
@@ -3,13 +3,13 @@ import s from './Environment.module.scss'
 import { myEnv } from '@/data'
 
 export default function Environment() {
-   const { computer, monitor, keyboard, mouse } = myEnv
+   const { computer, monitor, keyboard, mouse, image } = myEnv
 
    return (
       <div className={s.env}>
          <h2>My environment:</h2>
          <div className='flex flex-col md:flex-row md:items-start gap-5 md:gap-3'>
-            <Image src='/env.jpg' width={1000} height={600} alt='My environment' />
+            <Image src={image} width={1000} height={600} alt='My environment' />
             <ul className={s.list}>
                <li className={s.item}>
                   <h3>Computer</h3> {computer}
