db770221fbf9627883ee51b3c45b02e50f83bddc
Eduard Povierin <edpovieri@gmail.com>
2024-04-18 18:34:54 +0200

    Change gradient bg

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -36,7 +36,7 @@ const Layout = ({ title, description, children }) => {
             <ShaderGradient
                control='query'
                enableTransition={false}
-               urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.9&cAzimuthAngle=170&cDistance=4.2&cPolarAngle=70&cameraZoom=1&color1=%23a5b7f7&color2=%239caef3&color3=%238ba4eb&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=40&frameRate=10&gizmoHelper=hide&grain=off&lightType=env&pixelDensity=1&positionX=0&positionY=0.9&positionZ=-0.3&range=disabled&rangeEnd=39.5&rangeStart=2.4&reflection=0.5&rotationX=45&rotationY=0&rotationZ=0&shader=defaults&toggleAxis=false&type=waterPlane&uAmplitude=0&uDensity=0.9&uFrequency=0&uSpeed=0.35&uStrength=2.3&uTime=2.4&wireframe=false&zoomOut=false'
+               urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.4&cAzimuthAngle=170&cDistance=4.2&cPolarAngle=70&cameraZoom=1&color1=%23a5b7f7&color2=%239caef3&color3=%238ba4eb&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=40&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0.9&positionZ=-0.3&range=disabled&rangeEnd=39.5&rangeStart=2.4&reflection=0.5&rotationX=45&rotationY=0&rotationZ=0&shader=defaults&toggleAxis=false&type=waterPlane&uAmplitude=0&uDensity=0.9&uFrequency=0&uSpeed=0.33&uStrength=2.3&uTime=2.4&wireframe=false&zoomOut=false'
             />
          </ShaderGradientCanvas>
          <div className='bg-background min-h-screen w-screen fixed z-0 top-0 left-0'></div>
